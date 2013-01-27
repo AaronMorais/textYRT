26a1aa9bfd18307bbaba2117e07b3520a4f58f57
Aaron Morais <aaron.m@live.ca>
2013-01-27 12:13:31 -0500

    Update twilio library.

--- server.js
+++ server.js
@@ -1,2 +1,12 @@
 var keys = require('./keys');
-console.log(keys.SID());
+var express = require('express'),
+    app = express();
+var twilioAPI = require('twilio-api'),
+    cli = new twilioAPI.Client(keys.sid, keys.token);
+//OK... good so far. Now tell twilio-api to intercept incoming HTTP requests.
+app.use(cli.middleware() );
+//OK... now we need to register a Twilio application
+cli.account.getApplication(keys.sid, function(err, app) {
+    if(err) throw err; //Maybe do something else with the error instead of throwing?
+    app.register();
+});
