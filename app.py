b3622565f0e3a4e37427b427ad7d22b66dbad418
Aaron Morais <aaron.m@live.ca>
2013-01-27 17:33:25 -0500

    update reply

--- app.py
+++ app.py
@@ -3,19 +3,26 @@ import keys
 from flask import Flask, request
 from twilio.rest import TwilioRestClient
 
-client = TwilioRestClient(keys.sid, keys.token)
+    client = TwilioRestClient(keys.sid, keys.token)
 app = Flask(__name__)
 
-@app.route('/', methods=['GET', 'POST'])
-def home():
-    if request.method == 'POST':
-        smsSendResponse(request.Body, request.From)
-    return "Main Page."
+    @app.route('/', methods=['GET', 'POST'])
+    def home():
+        pageString = "Hi POSTMAN!"
+        if request.method == 'POST':
+            smsSendResponse(request.args.get('Body'))
+        else:
+            pageString = "Hello World!"
+        return pageString
 
-def smsSendResponse(incomingMessage, recipient):
-    responseBody = "Hi! You said: " + incomingMessage
-    call = client.sms.messages.create(to=recipient, from_=keys.phoneNumber, body=responseBody)
+    @app.route('/sendMessage/<messageBody>')
+    def sendMessage(messageBody):
+        return "Message " + messageBody + " has been sent."
 
-if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    def smsSendResponse(incomingMessage):
+        responseBody = incomingMessage
+        call = client.sms.messages.create(to="4168460453", from_=keys.phoneNumber, body=responseBody)
+
+    if __name__ == '__main__':
+        port = int(os.environ.get('PORT', 5000))
+        app.run(host='0.0.0.0', port=port)
