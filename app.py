5232a34c3af6d6cdaef9c440bd78d27fed1b5461
Aaron Morais <aaron.m@live.ca>
2013-01-27 17:35:12 -0500

    Fix indent

--- app.py
+++ app.py
@@ -3,26 +3,26 @@ import keys
 from flask import Flask, request
 from twilio.rest import TwilioRestClient
 
-    client = TwilioRestClient(keys.sid, keys.token)
+client = TwilioRestClient(keys.sid, keys.token)
 app = Flask(__name__)
 
-    @app.route('/', methods=['GET', 'POST'])
-    def home():
-        pageString = "Hi POSTMAN!"
-        if request.method == 'POST':
-            smsSendResponse(request.args.get('Body'))
-        else:
-            pageString = "Hello World!"
-        return pageString
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    pageString = "Hi POSTMAN!"
+    if request.method == 'POST':
+        smsSendResponse(request.args.get('Body'))
+    else:
+        pageString = "Hello World!"
+    return pageString
 
-    @app.route('/sendMessage/<messageBody>')
-    def sendMessage(messageBody):
-        return "Message " + messageBody + " has been sent."
+@app.route('/sendMessage/<messageBody>')
+def sendMessage(messageBody):
+    return "Message " + messageBody + " has been sent."
 
-    def smsSendResponse(incomingMessage):
-        responseBody = incomingMessage
-        call = client.sms.messages.create(to="4168460453", from_=keys.phoneNumber, body=responseBody)
+def smsSendResponse(incomingMessage):
+    responseBody = incomingMessage
+    call = client.sms.messages.create(to="4168460453", from_=keys.phoneNumber, body=responseBody)
 
-    if __name__ == '__main__':
-        port = int(os.environ.get('PORT', 5000))
-        app.run(host='0.0.0.0', port=port)
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
