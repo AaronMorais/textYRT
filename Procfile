d3c544e4cb5df18a9323cca5ef57cf905735a792
Aaron Morais <aaron.m@live.ca>
2013-01-27 16:16:04 -0500

    Move from node to flask. Add hello world to app.py

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: node server.js
+web: python app.py
---
+++ app.py
@@ -0,0 +1,30 @@
+import os
+import keys
+from flask import Flask, request
+from twilio.rest import TwilioRestClient
+
+client = TwilioRestClient(keys.sid, keys.token)
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    pageString = "Hi POSTMAN!"
+    if request.method == 'POST':
+        smsSendResponse("Hello World")
+    else:
+        pageString = "Hello World!"
+    return pageString
+
+@app.route('/sendMessage/<messageBody>')
+def sendMessage(messageBody):
+#    smsSendResponse(messageBody)
+    return "Message " + messageBody + " has been sent."
+
+def smsSendResponse(incomingMessage):
+    responseBody = incomingMessage
+    call = client.sms.messages.create(to="4168460453", from_=keys.phoneNumber, body=responseBody)
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.debug = True
+    app.run(host='0.0.0.0', port=port)
