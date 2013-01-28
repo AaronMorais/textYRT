af9da207441e58b26c19eb5eff864e69b5c76996
Aaron Morais <aaron.m@live.ca>
2013-01-27 19:06:02 -0500

    Remove custom message endpoint. Add checking for input string size. Change echo message.

--- app.py
+++ app.py
@@ -9,20 +9,13 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
-    body = "Hello "
-    body += str(request.args.get('Body'))
+    requestBody = str(request.args.get('Body'))
+    body = requestBody + " Results: Coming Soon!"
+    if(len(requestBody) != 4):
+        body = "Stop " + requestBody + " does not exist."
     resp = twilio.twiml.Response()
     resp.sms(body)
     return str(resp)
-
-@app.route('/sendMessage/<messageBody>')
-def sendMessage(messageBody):
-    smsSendResponse(messageBody)
-    return "Message " + messageBody + " has been sent."
-
-def smsSendResponse(incomingMessage):
-    responseBody = incomingMessage
-    call = client.sms.messages.create(to="4168460453", from_=keys.phoneNumber, body=responseBody)
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
